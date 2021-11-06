020d48195771b0936d0051f053be51215e659188
vira.murray <vira.avramenko@gmail.com>
2021-11-06 17:13:22 +0200

    mit capstone projects requirements

--- src/component/About.js
+++ src/component/About.js
@@ -3,8 +3,16 @@ import React, {Component} from "react";
 export default class About extends Component{
     render(){
         return(
-            <div>
-                <h1>About</h1>
+            <div id="resto">
+                <a href="https://www.evernote.com/shard/s739/sh/a542711f-58d6-b3c7-d76b-c17ea0bb339b/1a2e336ea31d5e9d154d675bd873c4ba">Full-Stack Restaurant Application Intro</a>
+                <br/>
+                <a href="https://www.evernote.com/shard/s739/sh/ddfe201b-b472-6d38-43b5-e01a816f5fb2/e12c1c8cbbe25ffec40b31ff04c2456e">Full-Stack Restaurant Application Presentation Submission</a> 
+                <br/>
+                <a href="https://www.evernote.com/shard/s739/sh/78c85442-18ea-e7c2-ec13-91283cfa6c93/927628d7cce3d74135d9753dd9415dd8">Full-Stack Restaurant Application Link Submission</a> 
+                <br/>
+                <a href="https://www.evernote.com/shard/s739/sh/5e2ff867-0ba2-d18d-5261-81bad3dfbf09/42406cdd5373eb7f982c4552722241a6">Restaurant App Presentation Capstone Rubric</a>
+
+
             </div>
         )
     }
--- src/component/NavbarComp.js
+++ src/component/NavbarComp.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Contacts from "./Contacts";
 import Home from "./Home";
 import About from "./About"
 
@@ -18,20 +17,22 @@ export default class NavbarComp extends Component {
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
 
-        {/* <Nav.Link as={Link} to={"/home"}>Home</Nav.Link>
+       <Nav.Link id="bank-app" as={Link} to={"/home"}>Full-Stack Banking Application</Nav.Link>
 
-        <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+        <Nav.Link id = "resto-app" as={Link} to={"/about"}>Full-Stack Restaurant Application</Nav.Link>
 
-        <Nav.Link as={Link} to={"/contacts"}>Contact</Nav.Link> */}
+        {/* <Nav.Link as={Link} to={"/contacts"}>Contact</Nav.Link>  */}
+        
+        <NavDropdown />  
 
-        <NavDropdown title="Full-Stack Banking App" id="basic-nav-dropdown">
+        {/* <NavDropdown title="Full-Stack Banking App" id="basic-nav-dropdown">
           <NavDropdown.Item target="_blank" href="https://www.evernote.com/shard/s739/client/snv?noteGuid=a97c2f6f-1811-9e63-5eed-0b392430a63a&noteKey=5a8756e3a0c43f28f1a0ae40a36c5319&sn=https%3A%2F%2Fwww.evernote.com%2Fshard%2Fs739%2Fsh%2Fa97c2f6f-1811-9e63-5eed-0b392430a63a%2F5a8756e3a0c43f28f1a0ae40a36c5319&title=1%2529%2BCapstone%2BOption%2B1%253A%2BFull-Stack%2BBanking%2BApplication%2BIntroduction%2BAnd%2BInstructions%2B%25285%253A00%2529">Introduction</NavDropdown.Item>
           <NavDropdown.Item target="_blank" href="https://www.evernote.com/shard/s739/client/snv?noteGuid=f954ece5-e0c7-3804-00d8-c7789f6e98b9&noteKey=ae699e0753ebb19a4a2d78004c13100b&sn=https%3A%2F%2Fwww.evernote.com%2Fshard%2Fs739%2Fsh%2Ff954ece5-e0c7-3804-00d8-c7789f6e98b9%2Fae699e0753ebb19a4a2d78004c13100b&title=2%2529%2BFull-Stack%2BBanking%2BApplication%2BPresentation%2BSubmission">Presentation Submition</NavDropdown.Item>
           <NavDropdown.Item target="_blank" href="https://www.evernote.com/shard/s739/client/snv?noteGuid=08e8f22b-dd7e-acb4-6d50-1305f153f662&noteKey=15ff4f9ee2dac6c2e074177eabf02aee&sn=https%3A%2F%2Fwww.evernote.com%2Fshard%2Fs739%2Fsh%2F08e8f22b-dd7e-acb4-6d50-1305f153f662%2F15ff4f9ee2dac6c2e074177eabf02aee&title=3%2529%2BFull-Stack%2BBanking%2BApplication%2BLink%2BSubmission">Link Submission</NavDropdown.Item>
           <NavDropdown.Item target="_blank" href="https://www.evernote.com/shard/s739/client/snv?noteGuid=0cb2fea3-3e7f-8ee2-1e11-f074ee45f4a4&noteKey=460f883f1f7b65be39e8e88fb008d45d&sn=https%3A%2F%2Fwww.evernote.com%2Fshard%2Fs739%2Fsh%2F0cb2fea3-3e7f-8ee2-1e11-f074ee45f4a4%2F460f883f1f7b65be39e8e88fb008d45d&title=Full-Stack%2BBanking%2BApplication%2BPresentation%2BSubmission%2Brubricn">Submission rubric</NavDropdown.Item>
                     <NavDropdown.Divider />
           <NavDropdown.Item target="_blank" href="https://www.evernote.com/shard/s739/client/snv?noteGuid=31a77fb3-ccdc-4a9e-25f7-5c1c450aa3ad&noteKey=ca7edf8f051f6d2c438fbfa5b19241ec&sn=https%3A%2F%2Fwww.evernote.com%2Fshard%2Fs739%2Fsh%2F31a77fb3-ccdc-4a9e-25f7-5c1c450aa3ad%2Fca7edf8f051f6d2c438fbfa5b19241ec&title=Things%2BI%2Balready%2Bworked%2Bon">Things I already worked on</NavDropdown.Item>
-        </NavDropdown>
+        </NavDropdown> */}
       </Nav>
     </Navbar.Collapse>
   </Container>
@@ -45,9 +46,9 @@ export default class NavbarComp extends Component {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/contacts">
+          {/* <Route path="/contacts">
             <Contacts />
-          </Route>
+          </Route> */}
          
         </Switch>
          </div>
